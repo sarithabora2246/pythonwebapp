6f5072f5d472c72a832fe32f21956fe3047b64c0
sarithabora2246 <133813131+sarithabora2246@users.noreply.github.com>
2024-08-14 18:40:27 +0000

    added the jobs and API route

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': '10L'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India'
+}, {
+    'id': 3,
+    'title': 'Front End Developer',
+    'location': 'Remote',
+    'salary': '20L'
+}]
 
 
 @app.route("/")
 def helloworld():
-  return "Hello Vasu"
+  return render_template('home.html', jobs=JOBS, companyname='Jovian')
 
 
-print(__name__)
+@app.route("/api/jobs")
+def getjobs():
+  return jsonify(JOBS)
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
